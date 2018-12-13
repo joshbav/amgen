6ccdf68395833e50d624b1680d7cad3ae9e37e23
“joshbav” <spam@nowhere.com>
2018-12-12 16:40:39 -0800

    scripted commit Wed-Dec-2018-04-40-39

--- startup.sh
+++ startup.sh
@@ -2,5 +2,10 @@ mkdir /data
 echo
 echo "Mounting NFS v4 AWS EFS mount"
 echo
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-06893bae.efs.us-west-2.amazonaws.com:/ /data
+mount -t nfs4 -o $NFS4-OPTIONS $NFS4-SERVER:$NFS4-SERVER-MOUNT $NFS4-LOCAL-MOUNTPOINT
+echo
+echo "Doing an LS of $NFS4-LOCAL-MOUNTPOINT"
+ls $NFS4-LOCAL-MOUNTPOINT
+echo
 sleep 100000000
+
